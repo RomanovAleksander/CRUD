083fdc340d37f023658ffa34ea4605a0eab7fbc1
RomanovAleksander <1eksandr999@gmail.com>
2021-10-20 13:51:42 +0300

    users component created, styles updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
+import Users from "../Users/Users";
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <Dashboard />
+      <Users />
     </div>
   );
 }
---
+++ src/components/Users/Users.js
@@ -0,0 +1,38 @@
+import './users.css';
+
+const Users = () => {
+  return (
+    <div className="content-wrapper">
+      <div className="page-title">Users:</div>
+      <div className="users-info">
+        <div className="users-info__block">
+          <p className="user-title">1White</p>
+          <p className="user-email">danilo.bilyi@gmail.com</p>
+          <p className="user-profiles">3 profiles</p>
+        </div>
+        <div className="users-info__block">
+          <p className="user-title">1White</p>
+          <p className="user-email">mail@mail.com</p>
+          <p className="user-profiles">1 profile</p>
+        </div>
+        <div className="users-info__block">
+          <p className="user-title">1White</p>
+          <p className="user-email">mail@mail.com</p>
+          <p className="user-profiles">1 profile</p>
+        </div>
+        <div className="users-info__block">
+          <p className="user-title">1White</p>
+          <p className="user-email">mail@mail.com</p>
+          <p className="user-profiles">1 profile</p>
+        </div>
+        <div className="users-info__block">
+          <p className="user-title">1White</p>
+          <p className="user-email">mail@mail.com</p>
+          <p className="user-profiles">1 profile</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Users;
