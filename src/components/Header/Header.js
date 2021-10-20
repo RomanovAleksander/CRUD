00a563609f1878d7115d43d85069f9442c788b7b
RomanovAleksander <1eksandr999@gmail.com>
2021-10-20 13:07:15 +0300

    header component created, styles and markup added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './header.css';
+import avatar from '../../assets/avatar.png';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="avatar-block">
+        <img className="avatar" style={{
+          backgroundImage: `url(${avatar})`
+        }}/>
+        <span className="name">1White</span>
+      </div>
+      <div className="buttons-wrapper">
+        <nav className="nav-menu">
+          <div className="nav-button">Profiles</div>
+          <div className="nav-button">Dashboard</div>
+          <div className="nav-button">Users</div>
+        </nav>
+        <div className="log-out">Log Out</div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
