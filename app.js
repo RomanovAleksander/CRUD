44c33b0d8d27c0c74368f6cdd11c96247394c46e
RomanovAleksander <1eksandr999@gmail.com>
2021-10-27 22:48:17 +0300

    server updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/user', require('./routes/user.routes'));
 app.use('/api/profile', require('./routes/profile.routes'));
 
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
   app.get('*', (req, res) => {
