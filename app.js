02788ef70dec1ad146d08d60e982845fb42fedf2
RomanovAleksander <1eksandr999@gmail.com>
2021-10-27 22:06:20 +0300

    server updated

--- app.js
+++ app.js
@@ -5,18 +5,29 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.json({ extended: true }));
 
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/user', require('./routes/user.routes'));
 app.use('/api/profile', require('./routes/profile.routes'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get('mongoUri'));
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
     app.listen(PORT, () => console.log(`App has been started on ${PORT}...`));
   } catch (e) {
     console.log('Server Error', e.message);
