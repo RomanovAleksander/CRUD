52faff432b065e826e84b0bd1df4c8b79a70b1e7
RomanovAleksander <1eksandr999@gmail.com>
2021-10-28 12:20:40 +0300

    scss variables added

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './components/App/App';
-import './index.scss';
+import './styles/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
