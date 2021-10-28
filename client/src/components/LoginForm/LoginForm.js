86933e48fa5b85a3d266b8d8701f3609e8792d30
RomanovAleksander <1eksandr999@gmail.com>
2021-10-28 13:38:54 +0300

    project structure improved

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,72 +1,11 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {useHistory} from 'react-router-dom';
-import {AuthContext} from '../../context/AuthContext';
-import { ToastContainer, toast } from 'react-toastify';
-import {useHttp} from '../../hooks/http.hook';
+import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import styles from './LoginForm.module.scss';
 
-const LoginForm =  ({ isSignIn }) => {
-  const defaultData = {
-    username: '',
-    email: '',
-    password: '',
-    isAdmin: false
-  };
-  const history = useHistory();
-  const auth = useContext(AuthContext);
-  const {loading, request, error, clearError} = useHttp();
-  const [userData, setUserData] = useState(defaultData);
-
-  useEffect(() => {
-    showToast(error, 'error');
-    clearError();
-  }, [error, clearError])
-
-  const showToast = (message, type) => {
-    toast[`${type}`](message, {
-      position: "top-right",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "dark"
-    });
-  }
-
-  const handleChange = (event) => {
-    setUserData(prevState => {
-      return {
-        ...prevState,
-        [event.target.name]: event.target.value
-      }
-    });
-  }
-
-  const handleCheck = (event) => {
-    setUserData({ ...userData, isAdmin: event.target.checked });
-  }
-
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    try {
-      if (!isSignIn) {
-        const data = await request('/api/auth/register', 'POST', {...userData});
-        showToast(data.message, 'success');
-        history.push('/');
-        setUserData({...defaultData});
-      } else {
-        const data = await request('/api/auth/login', 'POST', {
-          email: userData.email,
-          password: userData.password
-        });
-        auth.login(data.token);
-      }
-    } catch (e) {}
-  }
-
+const LoginForm =  ({ isSignIn, userData,
+                      loading, submitHandler,
+                      handleCheck, handleChange
+                    }) => {
   return (
     <div className={styles.wrapper}>
       <ToastContainer />
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,7 +1,78 @@
+import React, {useContext, useEffect, useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import {AuthContext} from '../context/AuthContext';
+import {useHttp} from '../hooks/http.hook';
 import LoginForm from '../components/LoginForm/LoginForm';
 
 export const AuthPage = ({ isSignIn }) => {
+  const defaultData = {
+    username: '',
+    email: '',
+    password: '',
+    isAdmin: false
+  };
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+  const {loading, request, error, clearError} = useHttp();
+  const [userData, setUserData] = useState(defaultData);
+
+  useEffect(() => {
+    showToast(error, 'error');
+    clearError();
+  }, [error, clearError])
+
+  const showToast = (message, type) => {
+    toast[`${type}`](message, {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark"
+    });
+  }
+
+  const handleChange = (event) => {
+    setUserData(prevState => {
+      return {
+        ...prevState,
+        [event.target.name]: event.target.value
+      }
+    });
+  }
+
+  const handleCheck = (event) => {
+    setUserData({ ...userData, isAdmin: event.target.checked });
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      if (!isSignIn) {
+        const data = await request('/api/auth/register', 'POST', {...userData});
+        showToast(data.message, 'success');
+        history.push('/');
+        setUserData({...defaultData});
+      } else {
+        const data = await request('/api/auth/login', 'POST', {
+          email: userData.email,
+          password: userData.password
+        });
+        auth.login(data.token);
+      }
+    } catch (e) {}
+  }
+
   return (
-    <LoginForm isSignIn={isSignIn} />
+    <LoginForm submitHandler={submitHandler}
+               handleCheck={handleCheck}
+               handleChange={handleChange}
+               userData={userData}
+               isSignIn={isSignIn}
+               loading={loading}
+    />
   )
 }
