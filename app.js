7ee979cf78e242d04a07d038c270a99d2e763e54
RomanovAleksander <1eksandr999@gmail.com>
2021-10-28 10:35:55 +0300

    server updated

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ app.use('/api/user', require('./routes/user.routes'));
 app.use('/api/profile', require('./routes/profile.routes'));
 
 if (config.get('NODE_ENV') === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  const root = path.join(__dirname, 'client', 'build');
+
+  app.use(express.static(root));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile('index.html', { root });
   })
 }
 
