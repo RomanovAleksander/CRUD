23b4c34c59a45fc069b1c8ea28a438958b9a6e9b
RomanovAleksander <1eksandr999@gmail.com>
2021-11-01 17:30:05 +0200

    Loader test updated

--- client/src/components/Loader/Loader.test.js
+++ client/src/components/Loader/Loader.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Loader } from './Loader';
 
-it('should render userList component', () => {
+it('should render Loader component', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Loader />, div);
   ReactDOM.unmountComponentAtNode(div);
